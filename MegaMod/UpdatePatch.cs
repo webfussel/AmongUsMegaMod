25079425d14403864ced631da9e1b48dbc4eb539
Dacendaran <57763867+Dacendaran@users.noreply.github.com>
2021-02-22 13:30:02 +0100

    Fixed small error with tracker

--- MegaMod/UpdatePatch.cs
+++ MegaMod/UpdatePatch.cs
@@ -104,7 +104,7 @@ namespace MegaMod
                         break;
                     case Tracker tracker:
                         tracker.CheckMarkButton(__instance);
-                        tracker.sabotageActive = sabotageActive;
+                        tracker.SetSabotageState(sabotageActive);
                         break;
                 }
             }
