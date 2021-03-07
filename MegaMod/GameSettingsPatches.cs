252266b561cd814ff6a45d620c86761950591802
Fiona Toma <fiona.lena.toma@gmail.com>
2021-03-07 17:33:02 +0100

    FIX: Hud Font Size

--- MegaMod/GameSettingsPatches.cs
+++ MegaMod/GameSettingsPatches.cs
@@ -1,15 +1,22 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 namespace MegaMod
 {
-    [HarmonyPatch(typeof(GameSettingMenu), nameof(GameSettingMenu.OnEnable))]
-    public class GameSettingsPatches
-    {
-        [HarmonyPatch(typeof(GameOptionsMenu), nameof(GameOptionsMenu.Update))]
-        class Update {
-            static void Postfix(ref GameOptionsMenu __instance) {
-                __instance.GetComponentInParent<Scroller>().YBounds.max = 16f;
-            }
+    
+    [HarmonyPatch(typeof(HudManager), nameof(HudManager.Update))]
+    class GameSettingsPatch {
+        static void Postfix(HudManager __instance)
+        {
+            __instance.GameSettings.scale = 0.43f;
+        }
+    }
+    
+        
+    [HarmonyPatch(typeof(GameOptionsMenu), nameof(GameOptionsMenu.Update))]
+    class Update {
+        static void Postfix(ref GameOptionsMenu __instance) {
+            __instance.GetComponentInParent<Scroller>().YBounds.max = 16f;
         }
     }
 }
